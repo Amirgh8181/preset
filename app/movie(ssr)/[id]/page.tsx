4d12d8f4132af6c8d7e18d0f5809cc37437a552e
amir.gh <amir.962@yahoo.com>
2023-09-30 14:19:14 +0330

    create dynamic metadata in ssr movie

--- app/movie(ssr)/[id]/page.tsx
+++ app/movie(ssr)/[id]/page.tsx
@@ -1,42 +1,30 @@
 import Image from "next/image";
 import NotFound from "../../not-found";
 import { Metadata,ResolvingMetadata } from "next";
+import { getOneData } from "../../../lib/getmovie";
 
 
-async function getData(id: number) {
-    try {
-        const req = await fetch(`https://moviesapi.ir/api/v1/movies/${id}`, { cache: 'no-store' })
-        return req.json()
-    }
-    catch (e) {
-        console.log(e);
-
-    }
-}
+// dynamic metadata
 export async function generateMetadata(
     { params, searchParams }: {params:{id:number},searchParams:string},
     parent: ResolvingMetadata
   ): Promise<Metadata> {
-    let res;
-    const req = await fetch(`https://moviesapi.ir/api/v1/movies/${params.id}`, { cache: 'no-store' })
-    .then(res=>res.json())
-    .then()
+    
+    const res = await getOneData(params.id)
+    
     return{
         title:res.title
     }
   }
 
 const MovieDetails = async ({ params,searchParams }:{params:{id:number},searchParams:string}) => {
-    let request = await getData(params.id)
+    let request = await getOneData(params.id)
     console.log(request);
-    console.log(params);
-    console.log(searchParams);
+    
     // agar id ersali dar javab ma vojood nadasht not found ra neshan bede
     if(!request.id){
         return NotFound()
     }
-    
-        
     return (
         <div className="container mx-auto bg-zinc-300 flex flex-row justify-between my-10 p-10 rounded-lg">
             <div className="flex flex-col">
@@ -52,3 +40,4 @@ const MovieDetails = async ({ params,searchParams }:{params:{id:number},searchPa
 }
 
 export default MovieDetails
+
